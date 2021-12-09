64ced05ccc163dd04044ff978f78017fb3b15fba
hsn8najafi <hsn8najafi@gmail.com>
2021-12-10 01:52:11 +0330

    Remove Contact Module

--- utils/contacts.js
+++ utils/contacts.js
@@ -10,3 +10,9 @@ exports.addContact = (fullname, phone, email) => {};
  * Show Contacts As a Simple Table
  */
 exports.listContacts = () => {};
+
+/**
+ * Filter Contact By Name And Remove From All Contacts List
+ * @param {string} fullname
+ */
+exports.removeContact = (fullname) => {};
