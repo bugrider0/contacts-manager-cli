7e21ce43e99afea8bb013c7b58088a9c02b55fbb
hsn8najafi <hsn8najafi@gmail.com>
2021-12-10 01:48:58 +0330

    addContact Module

---
+++ utils/contacts.js
@@ -0,0 +1,7 @@
+/**
+ * Basic Authenticate And New Contact
+ * @param {string} fullname
+ * @param {number} phone
+ * @param {string} email
+ */
+exports.addContact = (fullname, phone, email) => {};
